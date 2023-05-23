94b2f80c216fdad02a30eda8dbcb2c62bdd1b47c
EdwardZhanghk <110289915+EdwardZhanghk@users.noreply.github.com>
2023-05-23 14:57:41 +0800

    Update jandan-dark-theme.js

--- jandan-dark-theme.js
+++ jandan-dark-theme.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         煎蛋暗黑模式
+// @name         煎蛋暗黑模式测试
 // @namespace    https://github.com/EdwardZhanghk/jandan-dark-theme
-// @version      0.2.0
+// @version      0.3.0
 // @description  一个对调了煎蛋网文字和背景颜色的脚本，让煎蛋网拥有暗黑模式，妈妈再也不用担心我半夜摸鱼看煎蛋亮瞎眼了
 // @author       EdwardZhanghk
 // @match        http://jandan.net/*
@@ -9,85 +9,67 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    let css=`
-        body,html{
-            background:#111;
-            color:#B4B4B4;
-        }
-        #content .title {
-            background-color: #111;
-        }
-        #content .post blockquote {
-            background-color: #111;
-            color:#B4B4B4;
-        }
-        #sidebar form input{
-            background-color: #9F9F9F;
-        }
-        #sidebar form{
-            border-bottom:#e5e5e5;
-            background-color: #111;
-        }
-        .hot-tabs .hot-tab {
-            background-color: #222;
-        }
-        #sidebar h3 {
-            background-color: #222;
-        }
-        #sidebar ul {
-            background-color: #222;
-        }
-        .tucao-author, .tucao-{
-            background-color: #222;
-        }
-        .tucao-content{
-            background-color: #222;
-            color:#B4B4B4;
-        }
-        .tucao-hot-title {
-            background-color: #222;
-        }
-        .tucao-row{
-           background-color: #222;
-        }
-        .jandan-tucao{
-            background-color: #222;
-        }
-        .tucao-hot{
-           background-color: #222;
+'use strict';
 
-        }
-        .acv_author{
-            background-color: #222;
-            color:#9f9f9f
-        } 
-        .wp-pagenavi a {
-            background-color: #222;
-            color:#9f9f9f
-        }
-        #commentform textarea,#content textarea,button{
-            background-color: #222;
-            color:#fff;
-        }
-        #commentform input{
-            background-color: #9F9F9F;
-        }
-        #commentform #submit{
-            background-color: #222;
-            border: 1px solid #222;
-            color:#fff
-        }
-        a, a:link, a:visited{
-            color:#B4B4B4;
-        }
-        .topic-content,.topic-author,.topic-function,.reply,.acv_comment{
-            background-color: #222;
-        }
-        
-    `
- 
-    GM_addStyle(css)
-    // Your code here...
-    
+const css =`
+    body, html {
+        background:#111;
+        color:#B4B4B4;
+    }
+    #content .title,
+    #content .post blockquote,
+    #sidebar form,
+    #sidebar h3,
+    #sidebar ul,
+    .tucao-author,
+    .tucao-content,
+    .tucao-hot-title,
+    .tucao-row,
+    .jandan-tucao,
+    .tucao-hot,
+    .acv_author,
+    .wp-pagenavi a,
+    .topic-content,
+    .topic-author,
+    .topic-function,
+    .reply,
+    .acv_comment {
+        background-color: #222;
+    }
+    #sidebar form input,
+    #commentform input {
+        background-color: #9F9F9F;
+    }
+    #sidebar form {
+        border-bottom:#e5e5e5;
+    }
+    .hot-tabs .hot-tab {
+        background-color: #222;
+    }
+    #commentform textarea,
+    #content textarea,
+    button {
+        background-color: #222;
+        color: #fff;
+    }
+    #commentform #submit {
+        background-color: #222;
+        border: 1px solid #222;
+        color: #fff;
+    }
+    a, a:link, a:visited {
+        color: #B4B4B4;
+    }
+    .tucao-author,
+    .tucao-content {
+        color: #B4B4B4;
+    }
+`;
+
+GM_addStyle(css);
 })();
+
+
+
+
+
